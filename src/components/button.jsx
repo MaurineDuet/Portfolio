5e3ff77f6facf5baa721bce48217e4fd0b2f5ad6
Maurine Duet <duet_maurine@MacBook-Air-de-Maurine.local>
2023-07-07 16:22:22 +0200

    Fin de la page de landing

--- src/components/button.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/basic.scss'
-
-function Button() {
-    return(
-        <button style={{backgroundColor: "#C2DCFF"}}>mon CV</button>
-    )
-}
-
-export default Button
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,13 +1,15 @@
 import '../styles/basic.scss'
 import '../styles/landing.scss'
 
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 //Images
 import ComputerFrame from '../assets/frame.png'
 import StarsBackground from '../assets/stars_background.jpeg'
 import ProfilePic from '../assets/astronaute.gif'
-
-//Boutons
-import Button from '../components/button'
+import Stars1 from '../assets/stars_1.svg'
+import Stars2 from '../assets/stars_2.svg'
 
 //Footer
 import LandingFooter from '../components/landing_footer'
@@ -22,16 +24,25 @@ function Landing() {
                   <div className='profile'>
                         <img src={StarsBackground} alt="fond d'écran étoiles" className='stars_background' />
 
+                        <div className='stars'>
+                        <img src={Stars1} alt="" />
+                        <img src={Stars2} alt="" />
+                        </div>
+
                         <div className='profile_infos'>
                               <img src={ProfilePic} alt="dessin d'astronaute" />
                               <h2>Maurine Duet</h2>
                         </div>
 
                         <div className='landing_buttons'>
-                        <Button></Button>
-                        <Button></Button>
-                  </div>
-                  
+                              <Link to='/cv' className='link_button' style={{ backgroundColor: "#C2DCFF" }}>
+                                    mon CV
+                              </Link>
+                              <Link to='/universe' className='link_button' style={{ backgroundColor: "#C2DCFF" }}>
+                                    mon univers
+                              </Link>
+                        </div>
+
                   </div>
 
 
