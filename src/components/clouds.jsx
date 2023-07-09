57fb2ebf1ac900fea3fa1f9dfed2fe5199b54997
Maurine Duet <duet_maurine@MacBook-Air-de-Maurine.local>
2023-07-09 15:01:22 +0200

    Continuation test nuages page de landing

--- src/components/clouds.jsx
+++ src/components/clouds.jsx
@@ -2,20 +2,28 @@ import '../styles/basic.scss'
 import '../styles/landing.scss'
 import '../styles/clouds.scss'
 
+import { useEffect } from 'react'
+
 //Images clouds
-import TransparentCloud from '../assets/transparent_cloud.svg'
 import PinkCloud from '../assets/pink_cloud.svg'
 import BlueCloud from '../assets/blue_cloud.svg'
 import YellowCloud from '../assets/yellow_cloud.svg'
 import LilacCloud from '../assets/lilac_cloud.svg'
 
 function Clouds() {
+
+    useEffect(() => {
+        const cloudImages = document.querySelectorAll('.transparent_c, .pink_c, .blue_c, .yellow_c, .lilac_c');
+        cloudImages.forEach((image) => {
+            image.classList.add('active');
+        });
+    }, [])
+
     return (
         <div className='clouds_background'>
-            <img src={TransparentCloud} alt="Nuage transparent" className='transparent_c'/>
-            <img src={YellowCloud} alt="Nuage jaune" className='yellow_c'/>
-            <img src={PinkCloud} alt="Nuage rose" className='pink_c'/>
-            <img src={BlueCloud} alt="Nuage bleu" className='blue_c' />
+            <img src={YellowCloud} alt="Nuage jaune" className='yellow_c' />
+            <img src={PinkCloud} alt="Nuage rose" className='pink_c' />
+           {/*  <img src={BlueCloud} alt="Nuage bleu" className='blue_c' /> */}
             <img src={LilacCloud} alt="Nuage lilas" className='lilac_c' />
         </div>
     )
