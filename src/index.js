c70af27e691f37c7961754bb50a6f964b37b8d4b
Maurine Duet <duet_maurine@MacBook-Air-de-Maurine.local>
2023-07-26 15:47:26 +0200

    Url

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import Error from './pages/error'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         
